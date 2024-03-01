3d52cbc64bd232cd2a5e0b2151054004772f4001
kruhsilpatel01 <krushilchabhadiya600@gmail.com>
2024-03-01 23:12:40 +0530

    create a new page room-page and complete all section but i have problem which mean connect toggle menu side menu connect with each other

--- demo.js
+++ demo.js
@@ -3,4 +3,36 @@ function toggleSidebar() {
     document.getElementById("sidebar").classList.toggle('active');
     document.getElementById("navbarNav").classList.toggle('demo');
 
+
+}
+
+function message_popup() {
+    var x = document.getElementById('date_check_in').value;
+    var y = document.getElementById('date_check_out').value;
+
+    alert(x + " check in date" + y + "check out date");
+}
+
+function toggleSidebar() {
+
+    document.getElementById("sidebar").classList.toggle('active');
+
+}
+
+
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+
+    document.getElementById("navbar").style.display = "none";
+
+    document.getElementById("navbar2").style.display = "block";
+
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+
+    document.getElementById("navbar").style.display = "block";
+
+    document.getElementById("navbar2").style.display = "none";
 }
